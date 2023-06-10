75fc75593e9b56d435dd09eaff3f0c0211025e7b
Sarit Ritwirune <sarit@elcolie.com>
2023-06-10 20:54:51 +0700

    Success use trained model from civitAI in second_try.py

--- try_civitai/second_try.py
+++ try_civitai/second_try.py
@@ -1,15 +1,16 @@
+"""
+Get safetensor from civitAI
+https://civitai.com/models/43331/majicmix-realistic
+"""
 from diffusers import StableDiffusionPipeline
 import torch
 torch.manual_seed(111)
 device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
 
-model_id = "runwayml/stable-diffusion-v1-5"
-pipe = StableDiffusionPipeline.from_pretrained(model_id, safety_checker=None).to(device)
-
-pipe.load_textual_inversion("./charturnerv2.pt", token="charturnerv2")
+pipe = StableDiffusionPipeline.from_ckpt("majicmixRealistic_v5.safetensors", safety_checker=None).to(device)
 
 prompt = "charturnerv2, multiple views of the same character in the same outfit, a character turnaround of a woman wearing a black jacket and red shirt, best quality, intricate details."
 
-result = pipe(prompt, num_inference_steps=500, num_images_per_prompt=4)
+result = pipe(prompt, num_inference_steps=1000, num_images_per_prompt=4)
 for idx, image in enumerate(result.images):
     image.save(f"character_{idx}.png")
