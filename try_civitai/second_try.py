5891bb2b89ba56307bd5047e3d595c10f526be5f
Sarit Ritwirune <sarit@elcolie.com>
2023-06-28 21:10:16 +0700

    Add lora.

--- try_civitai/second_try.py
+++ try_civitai/second_try.py
@@ -1,21 +1,36 @@
 """
 Get safetensor from civitAI
-https://civitai.com/models/43331/majicmix-realistic
 """
 from diffusers import StableDiffusionPipeline
 import torch
+
+from read_lora import load_lora_weights
+
 torch.manual_seed(111)
-device = torch.device("mps") if torch.backends.mps.is_available() else torch.device("cpu")
+device = "mps" if torch.backends.mps.is_available() else "cpu"
 
-pipe = StableDiffusionPipeline.from_ckpt("majicmixRealistic_v5.safetensors", load_safety_checker=False).to(device)
+for _multipler in range(1, 21):
+    for _guidance in range(1, 21):
+        for _eta in range(1, 21):
+            multiplier: float = 0.1 * _multipler
+            eta: float = 0.1 * _eta
+            guidance_scale: float = 0.1 * _guidance
 
-prompt = "A photo of rough collie, best quality"
+            # https://civitai.com/models/43331/majicmix-realistic
+            model_base = "majicmixRealistic_v6.safetensors"
+            pipe = StableDiffusionPipeline.from_ckpt(
+                model_base,
+                load_safety_checker=False)
 
-negative_prompt: str = "low quality"
-guidance_scale = 1
-eta = 0.0
-result = pipe(
-    prompt, num_inference_steps=30, num_images_per_prompt=8,
-    guidance_scale=1, negative_prompt=negative_prompt)
-for idx, image in enumerate(result.images):
-    image.save(f"character_{guidance_scale}_{eta}_{idx}.png")
+            # https://civitai.com/models/82098/add-more-details-detail-enhancer-tweaker-lora
+            pipe = load_lora_weights(pipe, "more_details_lora.safetensors", multiplier, device)
+            pipe.to(device)
+            prompt = """
+            best quality, 8K, HDR, highres, blurry background, bokeh:1.3, Photography, (RAW photo:1.2, photorealistic:1.4, masterpiece:1.3, best quality:1.3, ultra highres:1.2), (((pin light, backlighting))), (depth of field), sharp focus:1.4, (camera from distance), (super_detail, hyper_detail, finely_detailed) ADDBASE blue sky ADDROW blue see ADDROW sand beach BREAK 1woman standing on sand beach, front view person. full body, ((((mature woman, solo, glamorous)))), (((gigantic breasts:0.7, narrow waist, oiledwet, large ass, thin thighs))) BREAK high detailed skin, delicate, beautiful skin, solid circle eyes, detailed beautiful face, beautiful detailed, (detailed eyes), (detailed facial features), beautiful and clear eyes, detail eye pupil, lipgloss, blush, cheek, long hair, black hair, (((expression_face, hair over one eye, beautiful fingers, beautiful hands))) BREAK bikini, earrings, necklace, sandals
+            """
+            negative_prompt: str = "Negative prompt: ((((BadNegAnatomyV1-neg, badhandv4)))), EasyNegative, paintings, sketches, (worst quality:2), (low quality:2), (normal quality:2), lowres, normal quality, ((monochrome)), ((grayscale)), ((((skin spots, acnes, skin blemishes, age spot)))), (((water mark, username, signature, text))), (((extra fingers, extra legs, extra foot, missing body, missing face, missing arms, missing hands, missing fingers, missing legs, missing feet, missing toe, strange fingers, bad hands, fewer fingers, extra digit, fewer digits)))"
+            result = pipe(
+                prompt, num_inference_steps=50, num_images_per_prompt=10, eta=eta,
+                guidance_scale=guidance_scale, negative_prompt=negative_prompt)
+            for idx, image in enumerate(result.images):
+                image.save(f"zz/character_{multiplier}_{guidance_scale}_{eta}_{idx}.png")
