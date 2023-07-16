1f2d09c042d74f04a9ca36915822d97dff8da6db
Sarit Ritwirune <sarit@elcolie.com>
2023-07-16 21:27:35 +0700

    Good savepoint.

--- try_civitai/sasithorn_controlnet.py
+++ try_civitai/sasithorn_controlnet.py
@@ -27,18 +27,17 @@ from set_seed import seed_everything, resize_for_condition_image
 seed: int = 8888
 seed_everything(seed)
 out_dir: str = "sasithorn_bikini_beach"
-negative_prompt: str = "low resolution, blur, bad quality, distort, bad shape, skirt, full body dress, cloth cover legs, limbs, multiple legs, bad face, blue edge"
+negative_prompt: str = "nsfw, ng_deepnegative_v1_75t,badhandv4, (worst quality:2), (low quality:2), (normal quality:2), lowres,watermark, monochrome"
 device: str = "mps" if torch.backends.mps.is_available() else "cpu"
 print(device)
 
 # init_image = init_image.resize((512, 512))
-# strengths = [round(0.1 * _, 3) for _ in range(0, 11, 1)]
-# guidance_scales = [round(0.2 * _, 3) for _ in range(0, 11, 1)]
+strengths = [round(0.2 * _, 3) for _ in range(1, 6, 1)]
+guidance_scales = [round(0.2 * _, 3) for _ in range(36, 51, 1)]
 # eta_list = [round(0.2 * _, 3) for _ in range(0, 11, 1)]
-prompt = "a naked beach girl sitting on the beach. She crosses her legs."
-# combined_list = list(itertools.product(
-#     strengths, guidance_scales, eta_list, additional_prompts)
-# )
+prompt = "best quality, masterpiece, (photorealistic:1.4), 1girl, light smile, a girl sitting on the beach. She crosses her legs and wearing 2 piece string bikini"
+combined_list = list(itertools.product(strengths, guidance_scales))
+random.shuffle(combined_list)
 init_image = load_image("sources/sasithorn.jpeg")
 width, height = init_image.size
 # size_factor: float = 0.8
@@ -51,7 +50,7 @@ init_image = init_image.resize((new_width, new_height))
 generator = torch.Generator(device=device).manual_seed(seed)
 mask_image = load_image("sources/masked_sasithorn.png").resize((new_width, new_height))
 
-# random.shuffle(combined_list)
+
 
 def make_inpaint_condition(image, image_mask):
     image = np.array(image.convert("RGB")).astype(np.float32) / 255.0
@@ -66,62 +65,64 @@ def make_inpaint_condition(image, image_mask):
 
 def main() -> None:
     """Run main function."""
-    strength = 1
-    guidance_scale = 7.5
+    # strength = 1
+    # guidance_scale = 7.5
     eta = 0
-    add_prompt = prompt
-    print(strength, guidance_scale, eta, add_prompt)
-    filename: str = f"{out_dir}/{add_prompt}_{strength}_{guidance_scale}_{eta}_0.png"
-    try:
-        if not os.path.exists(filename):
-            control_image = make_inpaint_condition(init_image, mask_image)
+    for item in tqdm(combined_list):
+        strength, guidance_scale = item
+        add_prompt = prompt
+        print(strength, guidance_scale, eta, add_prompt)
+        filename: str = f"{out_dir}/{add_prompt}_{strength}_{guidance_scale}_{eta}_0.png"
+        try:
+            if not os.path.exists(filename):
+                control_image = make_inpaint_condition(init_image, mask_image)
 
-            controlnet = ControlNetModel.from_pretrained(
-                "lllyasviel/control_v11p_sd15_inpaint",
-            ).to(device)
-            pipe = StableDiffusionControlNetInpaintPipeline.from_pretrained(
-                # "runwayml/stable-diffusion-v1-5",  # Reference bad
-                "../ai_directory/majicmixRealistic_v6",  # Quite good
-                # "../ai_directory/MeinaV10",   # Anime
-                # "../ai_directory/perfectWorld_v4Baked",  # Ordinary
-                # "../ai_directory/chilloutmix_NiPrunedFp32Fix",  # It gives full bodysuit
-                controlnet=controlnet,
-                requires_safety_checker=False,
-                safety_checker=None
-            ).to(device)
-            pipe.requires_safety_checker = False
-            pipe.safety_checker = None
+                controlnet = ControlNetModel.from_pretrained(
+                    "lllyasviel/control_v11p_sd15_inpaint",
+                ).to(device)
+                pipe = StableDiffusionControlNetInpaintPipeline.from_pretrained(
+                    # "runwayml/stable-diffusion-v1-5",  # Reference bad
+                    "../ai_directory/majicmixRealistic_v6",  # Quite good
+                    # "../ai_directory/MeinaV10",   # Anime
+                    # "../ai_directory/perfectWorld_v4Baked",  # Ordinary
+                    # "../ai_directory/chilloutmix_NiPrunedFp32Fix",  # It gives full bodysuit
+                    controlnet=controlnet,
+                    requires_safety_checker=False,
+                    safety_checker=None
+                ).to(device)
+                pipe.requires_safety_checker = False
+                pipe.safety_checker = None
 
-            pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
+                pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
 
-            # generate image
-            result = pipe(
-                prompt=add_prompt,
-                negative_prompt=negative_prompt,
-                num_inference_steps=100,
-                generator=generator,
-                image=init_image,
-                mask_image=mask_image,
-                control_image=control_image,
-                num_images_per_prompt=2,
-                width=new_width,
-                height=new_height
-                # strength=strength,
-                # guidance_scale=guidance_scale,
-                # eta=eta,
-            )
-            for idx, image in enumerate(result.images):
-                filename: str = f"{out_dir}/{add_prompt}_{strength}_{guidance_scale}_{eta}_{idx}.png"
-                image.save(filename)
-        else:
-            print("File exists.")
-    except Exception as err:
-        print(err)
-        print(f"{filename} is impossible")
-        with open(f"{out_dir}/log.txt", "a") as file:
-            file.write(str(err) + "\n")
-            file.write(filename + "\n")
-            file.write("====================================" + "\n")
+                # generate image
+                result = pipe(
+                    prompt=add_prompt,
+                    negative_prompt=negative_prompt,
+                    num_inference_steps=100,
+                    generator=generator,
+                    image=init_image,
+                    mask_image=mask_image,
+                    control_image=control_image,
+                    num_images_per_prompt=2,
+                    width=new_width,
+                    height=new_height,
+                    strength=strength,
+                    guidance_scale=guidance_scale,
+                    # eta=eta,
+                )
+                for idx, image in enumerate(result.images):
+                    filename: str = f"{out_dir}/{add_prompt}_{strength}_{guidance_scale}_{eta}_{idx}.png"
+                    image.save(filename)
+            else:
+                print("File exists.")
+        except Exception as err:
+            print(err)
+            print(f"{filename} is impossible")
+            with open(f"{out_dir}/log.txt", "a") as file:
+                file.write(str(err) + "\n")
+                file.write(filename + "\n")
+                file.write("====================================" + "\n")
 
 
 if __name__ == "__main__":
