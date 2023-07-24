68b978c7f6d6e731a5db49e2dd475b669088d87e
Sarit Ritwirune <sarit@elcolie.com>
2023-07-24 20:59:10 +0700

    With lora combinations.

--- try_civitai/models_schedulers_loras.py
+++ try_civitai/models_schedulers_loras.py
@@ -18,16 +18,17 @@ from tqdm import tqdm
 
 from set_seed import seed_everything
 from read_lora import load_lora_weights_orig
+from solve_77_limits import get_pipeline_embeds
 
 human_name: str = "noname"
 out_dir: str = f"{human_name}"
-# device: str = "mps" if torch.backends.mps.is_available() else "cpu"
-device: str = "cpu"  # With this LoRA it can't run with mps. It raises RuntimeError: Invalid buffer size: 58.07 GB
+device: str = "mps" if torch.backends.mps.is_available() else "cpu"
+# device: str = "cpu"  # With this LoRA it can't run with mps. It raises RuntimeError: Invalid buffer size: 58.07 GB
 print(device)
 
 init_image = load_image(f"sources/{human_name}/noname.jpeg")
 width, height = init_image.size
-size_factor: float = 0.8
+size_factor: float = 0.5
 new_width, new_height = math.floor(width * size_factor / 8) * 8, math.floor(height * size_factor / 8) * 8
 
 init_image = init_image.resize((new_width, new_height))
@@ -54,7 +55,7 @@ def make_inpaint_condition(image, image_mask):
     return image
 
 
-base_prompt = ""
+base_prompt = "a half body girl in sex bikini leans against table,"
 additional_prompts = [
     "(masterpiece:1.2), best quality,PIXIV,Night scene, <lora:Night scene_20230715120543:1>",
     "Ultra-realistic 8k CG,masterpiece,best quality,(photorealistic:1.2),HDR,RAW photo, (film grain:1.1), ((Depth of field)),(looking at viewer:1.2), street, saigon, 1girl, pants,  (full body)), aodai, <lora:saigon:0.6>, <lora:aodai:0.7>, (masterpiece,best quality:1.5)"
@@ -62,22 +63,22 @@ additional_prompts = [
 negative_prompt: str = "EasyNegative, badhandsv5-neg,Subtitles,word,"
 strengths = [1, ]
 # guidance_scales = [round(0.1 * _, 3) for _ in range(70, 252, 2)]
-guidance_scales = [10, 15, 20, 30, 40]
+guidance_scales = [30, ]
 # eta_list = [0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.5, 2.0, 4, 6, 8, 10]
 eta_list = [1]
 
 models = [
-    ("stable-diffusion-v1-5", "runwayml/stable-diffusion-v1-5"),  # Reference bad
+    # ("stable-diffusion-v1-5", "runwayml/stable-diffusion-v1-5"),  # Reference bad
     # ("majicmixRealistic_v6", "../ai_directory/majicmixRealistic_v6"),  # Quite good
-    # ("MeinaV10", "../ai_directory/MeinaV10"),  # Anime
-    # ("perfectWorld_v4Baked", "../ai_directory/perfectWorld_v4Baked"),  # Ordinary
+    ("MeinaV10", "../ai_directory/MeinaV10"),  # Anime
+    ("perfectWorld_v4Baked", "../ai_directory/perfectWorld_v4Baked"),  # Ordinary
     # ("chilloutmix_NiPrunedFp32Fix", "../ai_directory/chilloutmix_NiPrunedFp32Fix"),
     # ("henmixrealV10_henmixrealV10", "../ai_directory/henmixrealV10_henmixrealV10"),
 ]
 schedulers = [
-    # ("LMSDiscreteScheduler", diffusers.schedulers.scheduling_lms_discrete.LMSDiscreteScheduler),
+    ("LMSDiscreteScheduler", diffusers.schedulers.scheduling_lms_discrete.LMSDiscreteScheduler),
     # ("DDIMScheduler", diffusers.schedulers.scheduling_ddim.DDIMScheduler),
-    # ("DPMSolverMultistepScheduler", diffusers.schedulers.scheduling_dpmsolver_multistep.DPMSolverMultistepScheduler),
+    ("DPMSolverMultistepScheduler", diffusers.schedulers.scheduling_dpmsolver_multistep.DPMSolverMultistepScheduler),
     # ("EulerDiscreteScheduler", diffusers.schedulers.scheduling_euler_discrete.EulerDiscreteScheduler),
     # ("PNDMScheduler", diffusers.schedulers.scheduling_pndm.PNDMScheduler),
     # ("DDPMScheduler", diffusers.schedulers.scheduling_ddpm.DDPMScheduler),
@@ -86,7 +87,7 @@ schedulers = [
 ]
 loras = [
     (None, None),
-    ("Night_scene", "../ai_files/loras/Night_scene_20230715120543.safetensors"),
+    # ("Night_scene", "../ai_files/loras/Night_scene_20230715120543.safetensors"),
     ("Saigon", "../ai_files/loras/Oldsaigon-v1.0.safetensors"),
 ]
 lora_multipliers = [0.5, 1.0]
@@ -99,7 +100,7 @@ combined_list = list(itertools.product(
 random.shuffle(combined_list)
 
 for item in tqdm(combined_list, total=len(combined_list)):
-    (model_name, model_dir), (scheduler_name, scheduler), (lora_name, lora_file), lora_multiplier, strength, guidance_scale, eta, prompt = item
+    (model_name, model_dir), (scheduler_name, scheduler), (lora_name, lora_file), lora_multiplier, strength, guidance_scale, eta, add_prompt = item
     controlnets = [
         ControlNetModel.from_pretrained(
             "lllyasviel/control_v11p_sd15_inpaint",
@@ -121,17 +122,20 @@ for item in tqdm(combined_list, total=len(combined_list)):
         make_inpaint_condition(init_image, mask_background_image),
     ]
 
-    filename: str = f"{out_dir}/{model_name}_{scheduler_name}_{lora_name}_{strength}_{guidance_scale}_{eta}_{base_prompt} {prompt[:20]}.png"
+    prompt: str = f"{base_prompt} {add_prompt}"
+    prompt_embeds, negative_prompt_embeds = get_pipeline_embeds(pipe, prompt, negative_prompt, device)
+
+    filename: str = f"{out_dir}/{model_name}_{scheduler_name}_{lora_name}_{lora_multiplier}_{strength}_{guidance_scale}_{eta}_{base_prompt}_{add_prompt[:20]}.png"
     print(f"{filename} is running")
     if not os.path.exists(filename):
         # generate image
         image = pipe(
-            f"{base_prompt} {prompt}",
+            prompt_embeds=prompt_embeds,
+            negative_prompt_embeds=negative_prompt_embeds,
             image=init_image,
             mask_image=mask_background_image,
             control_image=my_images[0],
-            negative_prompt=negative_prompt,
-            num_inference_steps=50,
+            num_inference_steps=100,
             generator=generator,
             eta=eta,
             strength=strength,
